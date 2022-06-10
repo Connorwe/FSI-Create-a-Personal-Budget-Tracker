a0fe3eede5aef84a415c3b51a44e28c1767a6dee
Connor <connor5741213@gmail.com>
2022-06-10 17:33:42 -0400

    first commit

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+window.prompt(weeklyExpenseQuestions[0])
+var answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(answer)
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+var answer=weeklyExpenses = weeklyExpenses + answer
+console.log(numberAnswer)
+//number not correct after prompt. and prompt repeats itself 3 times in browser//
+//testing git commit
+//hello
+//hello
+
+
